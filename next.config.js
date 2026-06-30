afb551d9f693e0a33048a250e13b7253623985a6
pistonpandu-dev <pistonpandu@gmail.com>
2026-06-30 18:22:43 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,12 +3,18 @@ const nextConfig = {
   images: {
     domains: ['firebasestorage.googleapis.com'],
   },
-  experimental: {
-    serverActions: true,
-  },
+  reactStrictMode: true,
+  swcMinify: true,
+  poweredByHeader: false,
+  compress: true,
+  optimizeFonts: true,
+  output: 'standalone',
   env: {
     NEXT_PUBLIC_BASE_API_URL: process.env.NEXT_PUBLIC_BASE_API_URL,
     NEXT_PUBLIC_SOCKET_URL: process.env.NEXT_PUBLIC_SOCKET_URL,
+  },
+  experimental: {
+    serverActions: true,
   },
 }
 
